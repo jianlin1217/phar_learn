dfb7a99e199eabfc6b8958989ff83cc7b5b46dcb
jian-lin <duoplay91095@gmail.com>
2020-09-11 09:49:23 +0800

    新創檔案測試
    pharbag.php  =  被打包的php
    phartest.php = 打包功能的php
    pharuse.php = 使用被打包的phar

---
+++ pharbag.php
@@ -0,0 +1,10 @@
+<?php
+    class objphar
+    {
+        public function sayOK(){
+          echo "OK";  
+        }
+    }
+  $testobj= new objphar();
+
+?>
--- phartest.php
+++ phartest.php
@@ -1,3 +1,10 @@
 <?php
     //githubDesktop
+    $pharfile = ("pharbag.php");
+
+    //路徑
+    $path = (''); 
+    $phar = new phar($phatfile,'');
+    $phar->setAlias('test.phar');
+
 ?>
